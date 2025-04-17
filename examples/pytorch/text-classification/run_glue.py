ecab702174d85c5447256279588c83aca3850f3f
Jeffrey Huynh <jthuynh@amazon.com>
2025-04-17 21:29:51 +0000

    Fix ValueError when using --eval_do_concat_batches=False with run_glue.py
    https://github.com/huggingface/transformers/issues/37593

--- examples/pytorch/text-classification/run_glue.py
+++ examples/pytorch/text-classification/run_glue.py
@@ -507,8 +507,15 @@ def main():
     # You can define your custom compute_metrics function. It takes an `EvalPrediction` object (a namedtuple with a
     # predictions and label_ids field) and has to return a dictionary string to float.
     def compute_metrics(p: EvalPrediction):
-        preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
-        preds = np.squeeze(preds) if is_regression else np.argmax(preds, axis=1)
+        if training_args.eval_do_concat_batches:
+            preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
+            preds = np.squeeze(preds) if is_regression else np.argmax(preds, axis=1)
+            labels = p.label_ids
+        else:
+            preds = p.predictions
+            preds = np.squeeze(preds) if is_regression else np.argmax(preds, axis=2)
+            preds = preds.flatten()
+            labels = np.array(p.label_ids).flatten()
         result = metric.compute(predictions=preds, references=p.label_ids)
         if len(result) > 1:
             result["combined_score"] = np.mean(list(result.values())).item()
